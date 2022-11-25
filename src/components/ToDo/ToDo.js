57105be5f70dfb998a44907c3e50562fa6090947
luba150278 <luba150278@gmail.com>
2022-11-25 19:14:00 +0200

    all

---
+++ src/components/ToDo/ToDo.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import Pagination from "react-bootstrap/Pagination";
+import { store } from "../../store";
+import { fetchItem } from "../../store/dispatches/item.dispatch";
+import AddItem from "../AddItem/AddItem";
+import Items from "../Items/Items";
+import styles from "./ToDo.module.scss";
+
+const itemOnPage = 5;
+function ToDo() {
+  const [items, setItems] = useState([]);
+  const [isItemsUpdate, setIsItemsUpdate] = useState(0);
+  const [active, setActive] = useState(1);
+
+  let numbers = [];
+  for (let number = 1; number <= Math.ceil(items.length / itemOnPage); number++) {
+    numbers.push(
+      <Pagination.Item key={number} active={number === active} onClick={() => setActive(number)}>
+        {number}
+      </Pagination.Item>
+    );
+  }
+
+  const updateItems = (isUpdate) => {
+    if (isUpdate) {
+      setIsItemsUpdate(isItemsUpdate + 1);
+    }
+  };
+
+  async function getItems() {
+    const data = await store.dispatch(fetchItem());
+
+    if (data.payload) {
+      setItems(data.payload);
+      return;
+    }
+  }
+
+  useEffect(() => {
+    getItems();
+  }, [isItemsUpdate]);
+
+  return (
+    <section>
+      <div className="container">
+        <h1 className={styles.title}>Додату нову задачу:</h1>
+        <div className={styles.todoWrap}>
+          <AddItem updateItems={updateItems} />
+          <Items items={items.slice((active - 1) * itemOnPage, active * itemOnPage)} updateItems={updateItems} />
+        </div>
+
+        <Pagination>{numbers}</Pagination>
+      </div>
+    </section>
+  );
+}
+
+export default ToDo;
